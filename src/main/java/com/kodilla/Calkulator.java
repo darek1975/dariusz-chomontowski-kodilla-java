9b0f4e7b2dfd5cd1a16c1fd85b951f25522a4697
darek1975 <dchomontowski@wp.pl>
2019-04-01 13:40:37 +0100

    First Commit

---
+++ src/main/java/com/kodilla/Calkulator.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+public class Calkulator {
+    {
+        public double addAToB(double a, double b) {
+        return a + b;
+    }
+
+        public double subtractAFromB(double a, double b) {
+        return a - b;
+    }
+    }
+}
