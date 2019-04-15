48b091062b2f35eac9d403729da87ec45639b61e
darek1975 <dchomontowski@wp.pl>
2019-04-15 18:42:28 +0100

    First Commit

--- kodilla-testing1/src/test/java/com/kodilla/testing/forum/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing1/src/test/java/com/kodilla/testing/forum/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -3,9 +3,12 @@ import com.kodilla.testing.collection.OddNumbersExterminator;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class CollectionTestSuite {
-    @Test
+import java.util.ArrayList;
 
+
+public class CollectionTestSuite {
+
+    @Test
     public void testCaseCollection(){
         //Given
         ArrayList<Integer> number = new ArrayList<Integer>();
@@ -20,15 +23,15 @@ public class CollectionTestSuite {
         number.add(57);
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
         ArrayList<Integer> evens = new ArrayList<Integer>();
-        number.add(50);
-        number.add(52);
-        number.add(54);
-        number.add(56);
+        evens.add(50);
+        evens.add(52);
+        evens.add(54);
+        evens.add(56);
 
         //When
         ArrayList<Integer> evensnumbers = exterminator.exterminate(number);
         //Then
-        Assert.assertEquals(ArrayList<Integer> evens, ArrayList<Integer> evensnumbers);
+        Assert.assertEquals(evens, evensnumbers);
     }
 
 }
