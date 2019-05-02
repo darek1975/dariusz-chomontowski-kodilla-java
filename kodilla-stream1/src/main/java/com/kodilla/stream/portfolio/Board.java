82aeffcac91330d1e39e30bdfbe9f79c1257b322
darek1975 <dchomontowski@wp.pl>
2019-05-02 11:51:53 +0100

    First Commit

---
+++ kodilla-stream1/src/main/java/com/kodilla/stream/portfolio/Board.java
@@ -0,0 +1,42 @@
+package com.kodilla.stream.portfolio;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    private final List<TaskList> taskLists = new ArrayList<>();
+    private final String name;
+
+    public Board(final String name) {
+        this.name = name;
+    }
+
+    public void addTaskList(TaskList taskList) {
+        taskLists.add(taskList);
+    }
+
+    public boolean removeTaskList(TaskList taskList) {
+        return taskLists.remove(taskList);
+    }
+
+    public List<TaskList> getTaskLists() {
+        return new ArrayList<>(taskLists);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public long diffInDays(LocalDate now, LocalDate t) {
+        return ChronoUnit.DAYS.between(now, t);
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" + "\n" +
+                "name='" + name + '\'' + ",\n" +
+                "taskLists=" + taskLists + "\n" +
+                '}';
+    }
+}
